280e77a4601f9ada38d7a4a08304ecd717c17764
IDrinkYourTea <legoemil123@gmail.com>
2022-11-17 10:15:33 +0100

    Add project files.

---
+++ Digit Int/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digit_Int
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nummer = 0, count = 0;
+            Console.Write("Skrive Nummer :");
+            nummer = Convert.ToInt32(Console.ReadLine());
+            while (nummer > 0)
+            {
+                nummer = nummer / 10;
+                count++;
+            }
+            Console.WriteLine("Nummer digit er : " + count);
+            Console.ReadLine();
+        }
+    }
+}
